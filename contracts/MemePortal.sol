6b70ebbc49fc6271ff89d83110818efdde8d97fd
Seow Li Chong Glen <glenseowlc@hotmail.com>
2022-02-07 19:12:00 +0800

    complete contract logic and happy path testing

--- contracts/MemePortal.sol
+++ contracts/MemePortal.sol
@@ -3,35 +3,69 @@
 pragma solidity ^0.8.4;
 
 import "hardhat/console.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract MemePortal {
-    event NewMeme(address indexed from, uint256 timestamp, string message);
+    using SafeMath for uint256;
+
+    uint256 id;
+
+    event NewMeme(uint256 id, address indexed from, uint256 timestamp, string message);
 
     struct Meme {
-        address waver;
+        address creator;
         string message;
         uint256 timestamp;
+        uint256 id;
     }
 
     Meme[] memes;
 
+    mapping(address => uint256[]) public approvedMemes;
+    mapping(address => uint256) public balances;
+
     constructor() payable {}
 
-    function sendMeme(string memory _message) public {
-        memes.push(Meme(msg.sender, _message, block.timestamp));
-        emit NewMeme(msg.sender, block.timestamp, _message);
+    function createMeme(string memory _message) public {
+        memes.push(Meme(msg.sender, _message, block.timestamp, id.add(1)));
+        emit NewMeme(id.add(1), msg.sender, block.timestamp, _message);
+        id = id.add(1);
+    }
+
+    function approveMeme(uint256 memeId, address memeCreator) external payable {
+        require(msg.value == 0.0001 ether, "Not exact deposit");
+        require(msg.sender.balance >= 0.0001 ether, "Approver has not enough ether.");
+        // TODO: use SafeMath for storage variable
+        balances[memeCreator] += msg.value;       
+        approvedMemes[msg.sender].push(memeId);
+    }
+
+    function withdrawTips() external payable {
+        require(
+            balances[msg.sender] <= address(this).balance,
+            "Trying to withdraw more money than the contract has."
+        );
+        (bool success, ) = (msg.sender).call{value: balances[msg.sender]}("");
+        require(
+            success, 
+            "Failed to withdraw money from contract."
+        );
+        balances[msg.sender] = 0;
     }
 
     function getAllMemes() public view returns (Meme[] memory) {
         return memes;
     }
-}
 
-// uint256 prizeAmount = 0.0001 ether;
-// require(
-//     prizeAmount <= address(this).balance,
-//     "Trying to withdraw more money than they contract has."
-// );
-// (bool success, ) = (msg.sender).call{value: prizeAmount}("");
-// require(success, "Failed to withdraw money from contract.");
+    function viewBalance() public view returns(uint256) {
+        return balances[msg.sender];
+    }
+
+    function viewApprovedMemes() public view returns(uint256[] memory) {
+        return approvedMemes[msg.sender];
+    }
+
+    function viewContractBalance() public view returns(uint) {
+        return address(this).balance;
+    }
+}
--- test/MemePortal.js
+++ test/MemePortal.js
@@ -24,35 +24,80 @@ describe("MemePortal contract", () => {
     });
   });
 
-  describe("sendMeme", () => {
+  describe("create and save meme logic", () => {
     it("should send and save memes successfully", async () => {
       await memeContract.deployed();
       
       let allMemes = await memeContract.getAllMemes();
       expect(allMemes).to.deep.equal([]);
 
-      await memeContract.sendMeme("test1");
+      await memeContract.createMeme("test1");
       allMemes = await memeContract.getAllMemes();
       
       expect(allMemes.length).to.equal(1);
-      expect(allMemes[0].waver).to.equal(owner.address);
+      expect(allMemes[0].id).to.equal(1);
+      expect(allMemes[0].creator).to.equal(owner.address);
       expect(allMemes[0].message).to.equal("test1");
       // Assume timestamp difference will have delta of +/- 100 ms
       expect(Math.floor(allMemes[0].timestamp / 100)).to.equal(Math.floor(currTimestamp / 100));
+
+      currTimestamp = Date.now() + 1234;
+      await ethers.provider.send("evm_mine", [currTimestamp]);
+
+      await memeContract.createMeme("test2");
+      allMemes = await memeContract.getAllMemes();
+      
+      expect(allMemes.length).to.equal(2);
+      expect(allMemes[1].id).to.equal(2);
+      expect(allMemes[1].creator).to.equal(owner.address);
+      expect(allMemes[1].message).to.equal("test2");
+      // Assume timestamp difference will have delta of +/- 100 ms
+      expect(Math.floor(allMemes[1].timestamp / 100)).to.equal(Math.floor(currTimestamp / 100));
     });
 
     it("should emit NewMeme successfully", async () => {
       await memeContract.deployed();
 
-      const tx = await memeContract.sendMeme("test2");
+      const tx = await memeContract.createMeme("test2");
       const receipt = await tx.wait();
-
       expect(receipt.events.length).to.equal(1);
+      expect(receipt.events[0].event).to.equal("NewMeme");
+
       const eventArgs = receipt.events[0].args;
-      expect(eventArgs["0"]).to.equal(owner.address);
+      expect(eventArgs["0"]).to.equal(1);
+      expect(eventArgs["1"]).to.equal(owner.address);
       // Assume timestamp difference will have delta of +/- 100 ms
-      expect(Math.floor(eventArgs["1"] / 100)).to.equal(Math.floor(currTimestamp / 100));
-      expect(eventArgs["2"]).to.equal("test2");
+      expect(Math.floor(eventArgs["2"] / 100)).to.equal(Math.floor(currTimestamp / 100));
+      expect(eventArgs["3"]).to.equal("test2");
+    });
+  });
+
+  describe("approve meme and withdraw funds logic", () => {
+    it("should complete happy path successfully", async () => {
+      await memeContract.deployed();
+      
+      await memeContract.createMeme("test1");
+      let contractBalance = await memeContract.viewContractBalance();
+      expect(ethers.utils.formatEther(contractBalance)).to.equal("0.1");
+
+      await memeContract.connect(addr1).approveMeme(1, owner.address, {value: ethers.utils.parseEther('0.0001')});
+      
+      contractBalance = await memeContract.viewContractBalance();
+      expect(ethers.utils.formatEther(contractBalance)).to.equal("0.1001");
+
+      let balance = await memeContract.viewBalance();
+      expect(ethers.utils.formatEther(balance)).to.equal("0.0001");
+
+      let approvedMemes = await memeContract.connect(addr1).viewApprovedMemes();
+      expect(approvedMemes.length).to.equal(1);
+      expect(approvedMemes[0]).to.equal(1);
+
+      let addrBalanceBefore = await ethers.provider.getBalance(owner.address);
+      await memeContract.withdrawTips();
+      balance = await memeContract.viewBalance();
+      expect(ethers.utils.formatEther(balance)).to.equal("0.0");
+      let addrBalanceAfter = await ethers.provider.getBalance(owner.address);
+      expect(parseInt(addrBalanceAfter)).to.greaterThan(parseInt(addrBalanceBefore));
     });
   });
 });
